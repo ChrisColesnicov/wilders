3b84d13bb75b96f32c8fd0f0e1a942f559c240ec
Christian Colesnicov <chris.colesnicov@gmail.com>
2024-06-04 10:47:58 +0200

    added chalk with names and colors

---
+++ index.js
@@ -0,0 +1,6 @@
+import chalk from 'chalk';
+
+console.log(chalk.bgRed('Hello Anais'));
+console.log(chalk.green('Hello Baptiste'));
+console.log(chalk.bgYellow('Hello Clément'));
+console.log(chalk.blue('Hello Kevin'));
